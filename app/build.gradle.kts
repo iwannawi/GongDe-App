a2e08f69fd9a69f48ebe9539a3100caf1ed87691
iwannawi <11927529+iwannawi@users.noreply.github.com>
2026-06-02 00:51:58 +0800

    bump: v1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.gongde.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.3.1"
     }
 
     buildTypes {
