4e0d3b359d4d80a4c7d7d222b125da62175caa70
iwannawi <11927529+iwannawi@users.noreply.github.com>
2026-06-17 21:07:35 +0800

    bump version to 2.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.gongde.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.4.0"
+        versionCode = 13
+        versionName = "2.4.1"
     }
 
     signingConfigs {
